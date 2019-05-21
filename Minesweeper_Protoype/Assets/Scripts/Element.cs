8f5f2255c0e3cf62fd7d0e08dcbb2f7a858033b0
Jose M. Puebla <joey_manuel@outlook.com>
2019-05-21 14:25:23 -0400

    4. LoadTexture
    Testing out how the function LoadTexture() works.

---
+++ Minesweeper_Protoype/Assets/Scripts/Element.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour 
+{
+	#region Variables
+
+	public Sprite[] emptyTextures;
+	public Sprite mineTexture;
+
+	// either a mine or not
+	public bool mine;
+
+	#endregion
+
+	private void Start() 
+	{
+		// Randomly decide if it's a mine or not
+//		mine = Random.value < 0.15;
+
+		// TEST
+		LoadTexture(1);
+
+		
+	}
+
+	public void LoadTexture(int adjacentCount)
+	{
+		if(mine)
+		{
+			// checks if the sprite is a texture or not.
+			GetComponent<SpriteRenderer>().sprite = mineTexture;
+		}
+		else
+		{
+			// else renders one of the empty texutures.
+			GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+		}
+	}
+
+
+
+
+} // main class
